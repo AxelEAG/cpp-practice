ba86fd25e11b2646476927f7780b4d8e9f689bbd
Axel Almaguer <axel.0619@hotmail.com>
2026-05-15 16:21:16 -0400

    Add bishop piece

---
+++ chess/bishop.cpp
@@ -0,0 +1,38 @@
+#include "Board.h"
+#include "Coord.h"
+#include "Bishop.h"
+#include <vector>
+
+std::vector<Coord> Bishop::getValidMoves(const Board& board, Coord position) const
+{
+	Side currSide{ board.getPiece(position)->getSide() };
+	std::vector<Coord> moves{};
+	auto walk = [&](int dx, int dy)
+		{
+			int x{ position.x + dx };
+			int y{ position.y + dy };
+
+			while (x >= 0 && x < Board::numCols
+				&& y >= 0 && y < Board::numRows)
+			{
+				Coord to{ x, y };
+				if (!board.isEmpty(to))
+				{
+					if (board.getPiece(to)->getSide() != currSide)
+						moves.push_back(to);
+					break;
+				}
+				moves.push_back(to);
+				x += dx;
+				y += dy;
+			}
+		};
+
+	walk( 1,  1);
+	walk(-1, -1);
+	walk( 1, -1);
+	walk(-1,  1);
+
+	return moves;
+
+}
