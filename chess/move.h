1e26bd0296242d40502f7b936f01d31d6f1a8efd
Axel Almaguer <axel.0619@hotmail.com>
2026-06-06 08:54:25 -0400

    Remove unnecessary information on Move struct. Incorporate flags to keep track of move types/categories. Add necessary helper functions

--- chess/move.h
+++ chess/move.h
@@ -4,39 +4,36 @@
 #include "piece.h"
 #include <optional>
 
-enum class Special
+enum MoveFlags
 {
 	none,
-	double_step,
-	en_passant,
-	promotion,
-	queenside_castle,
-	kingside_castle,
-};
+	
+	capture,
 
-enum class CastleSide
-{
-	kingside,
-	queenside
-};
+	enPassant ,
+	doublePush,
+	
+	kingCastle ,
+	queenCastle,
+	
+	promoteKnight,
+	promoteBishop,
+	promoteRook  ,
+	promoteQueen ,
 
-enum class Check
-{
-	none,
-	check,
-	checkmate,
+	capturePromoteKnight,
+	capturePromoteBishop,
+	capturePromoteRook  ,
+	capturePromoteQueen
 };
 
 struct Move
 {
-	Piece piece{};
 	Square from;
 	Square to;
-	bool takes{ false };
-	Check isCheck{ Check::none };
-	Special special{ Special::none };
-	Piece promote_to{ Piece::empty };
+	MoveFlags flags;
 };
+
 
 struct ParsedMove
 {
@@ -48,17 +45,122 @@ struct ParsedMove
 	Square from;
 	Square to;
 
-	bool takes{ false };
-	Check isCheck{ Check::none };
-	Special special{ Special::none };
-	PieceType promote_to{ PieceType::empty };
+	bool check{ false };
+	bool checkmate{ false };
+	MoveFlags flags{ none };
 };
 
 struct Undo
 {
-	Piece captured;
 	uint8_t castlingRights;
-	std::optional<Square> en_passant;
+	std::optional<Square> enPassant;
+	Square blackKingSq;
+	Square whiteKingSq;
+	Piece captured;
+
 };
 
+enum class CastleSide
+{
+	kingside,
+	queenside
+};
+
+constexpr bool operator==(const ParsedMove& move, MoveFlags flag)
+{
+	return move.flags == flag;
+}
+
+constexpr bool isPromotion(const ParsedMove& move)
+{
+	return move.flags >= promoteKnight;
+}
+
+constexpr bool isCastle(const ParsedMove& move)
+{
+	return (move.flags == kingCastle || move.flags == queenCastle);
+}
+
+constexpr bool operator==(const Move& move, MoveFlags flag)
+{
+	return move.flags == flag;
+}
+
+constexpr bool operator!=(const Move& move, MoveFlags flag)
+{
+	return move.flags != flag;
+}
+
+constexpr bool isPromotion(const Move& move)
+{
+	return move.flags >= promoteKnight;
+}
+
+constexpr bool isCapturePromotion(const Move& move)
+{
+	return move.flags >= promoteKnight;
+}
+
+constexpr MoveFlags toPromoteFlag(PieceType type)
+{
+	switch (type)
+	{
+	case PieceType::knight: 
+		return promoteKnight;
+	case PieceType::bishop:
+		return promoteBishop;
+	case PieceType::rook:
+		return promoteRook;
+	case PieceType::queen:
+		return promoteQueen;
+	default:
+		assert(0 && "toPromoteFlag: should not be called with unpromotable pieces");
+		return;
+	}
+}
+
+constexpr MoveFlags toCapturePromoteFlag(PieceType type)
+{
+	switch (type)
+	{
+	case PieceType::knight:
+		return capturePromoteKnight;
+	case PieceType::bishop:
+		return capturePromoteBishop;
+	case PieceType::rook:
+		return capturePromoteRook;
+	case PieceType::queen:
+		return capturePromoteQueen;
+	default:
+		assert(0 && "toCapturePromoteFlag: should not be called with unpromotable pieces");
+		return;
+	}
+}
+
+constexpr PieceType promotionPieceType(const Move& move)
+{
+	switch (move.flags)
+	{
+	case promoteKnight: 
+	case capturePromoteKnight:
+		return PieceType::knight;
+	case promoteBishop:
+	case capturePromoteBishop:
+		return PieceType::bishop;
+	case promoteRook:
+	case capturePromoteRook:
+		return PieceType::rook;
+	case promoteQueen:
+	case capturePromoteQueen:
+		return PieceType::queen;
+	default:
+		assert(0 && "promotionPiece: should not be called with non-promotion move");
+		return;
+	}
+}
+
+constexpr Piece promotionPiece(const Move& move, Side side)
+{
+	return toPiece(promotionPieceType(move), side);
+}
 #endif
