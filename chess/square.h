3e24eb1eef1c183f371cfe8b9a891444bf67c646
Axel Almaguer <axel.0619@hotmail.com>
2026-06-10 13:46:10 -0400

    Add enum for easier square manipulation

--- chess/square.h
+++ chess/square.h
@@ -4,6 +4,19 @@
 #include <array>
 #include <iostream>
 
+// TODO: Maybe eventually switch to just this int?
+enum Squaree
+{
+	A8, B8, C8, D8, E8, F8, G8, H8,
+	A7, B7, C7, D7, E7, F7, G7, H7,
+	A6, B6, C6, D6, E6, F6, G6, H6,
+	A5, B5, C5, D5, E5, F5, G5, H5,
+	A4, B4, C4, D4, E4, F4, G4, H4,
+	A3, B3, C3, D3, E3, F3, G3, H3,
+	A2, B2, C2, D2, E2, F2, G2, H2,
+	A1, B1, C1, D1, E1, F1, G1, H1,
+
+};
 struct Dir
 {
 	int x{};
@@ -42,6 +55,9 @@ struct Square
 	Rank rank{};
 
 	constexpr Square() = default;
+	constexpr Square(Squaree sq) : Square{ sq % 8, sq / 8 } {}
+	constexpr Square(int sq) : Square{ sq % 8, sq / 8 } {}
+
 	constexpr Square(File f, Rank r) : file{ f }, rank{ r } {}
 	constexpr Square(int f, int r) : file{ static_cast<File>(f) }, rank{ static_cast<Rank>(r) } {}
 	constexpr Square operator+(Dir dir) const
